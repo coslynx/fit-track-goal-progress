e219741297f6817b7ef6841f75d76871febc03a2
CosLynx AI <drixisgod@gmail.com>
2025-01-12 17:14:24 +0530

    generated file: `constants/apiEndpoints.js` [backend, utils]

---
+++ constants/apiEndpoints.js
@@ -0,0 +1,5 @@
+export const REGISTER_ENDPOINT = '/api/auth/register';
+export const LOGIN_ENDPOINT = '/api/auth/login';
+export const ME_ENDPOINT = '/api/auth/me';
+export const GOALS_ENDPOINT = '/api/goals';
+export const GOAL_ENDPOINT = '/api/goals/:id';
